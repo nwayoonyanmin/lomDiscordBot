293ab72a6ac9bd47fdd6310f928f32d6ec0214e4
nway oo nyan min <nwayoonyanmin@gmail.com>
2020-09-30 15:56:34 +0630

    cls

--- sayargyi.js
+++ sayargyi.js
@@ -35,55 +35,34 @@ bot.on('message', message => {
             case 'tatesan':
             case 'တိတ်စမ်း':
             case 'mutemute':
-                //return message.reply("This command excuted");
-                //message.channel.send(args[0]);
+
                 if(args.length < 2){return message.reply('ငါကဘယ်ကောင့်ကို Mute ရမှာလဲဟ');}
 
-                /*console.log(fullmsg);
-                console.log(getArgs(fullmsg,"-t"))   ;
-                console.log(getArgs(fullmsg,"-r"))   ;
-                return; */
                 var person  = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[1]));
-
-                /*if(message.member.roles.find()
-                {
-                    console.log('found!')
-                }
-                return;*/
 
                 if(!person) return  message.reply(  args[1] + " ဆိုတဲ့ကောင်မရှိဘူးဟ ") 
                 
                 console.log('user found!!');
-                //message.channel.send(args[1]);
 
                 let mainrole = message.guild.roles.cache.find(role => role.name === "newcomer");
                 let role = message.guild.roles.cache.find(role => role.name === "Laung-Kee");
             
-
                 if(!role) return message.reply("Couldn't find the mute role.")
                 console.log('role found!!');
 
                 //let time = args[2];
                 let time = getArgs(fullmsg,"-t");
                 if(time==-1){
-                    //return message.reply("You didnt specify a time!");
                     time = '5s'; // 5s by default for testing REASON !!!.
                 }
                 console.log('timer OK!!' + time);
                 //let desc = args[3];
                 let desc = getArgs(fullmsg,"-r");
                 if(desc==-1){
-                    //return message.reply("tell him why is he a Laung Kee")
                     desc = 'ဘလိုင်းကြီး Laung-Kee';
                 }
                 console.log('reasons OK!!' + desc);
 
-                //------- EXTER FUNCTION TESTED AND IT'S WORKED ---------
-                //console.log(getStr());
-                //message.channel.send(role.id);
-                
-                //person.roles.remove(mainrole.id);
-                //console.log(person.voice.channel);
                 try {
                     if(person.voice.channel.id==null){return;}
                 } catch (err) {
@@ -113,26 +92,18 @@ bot.on('message', message => {
                 
                 try {
                     setTimeout(function(){
-                
-                        //person.roles.add(mainrole.id);
+
                         person.voice.setMute(false);
                         person.roles.remove(role.id);
-                        //console.log(role.id)
                         message.channel.send(`<@${person.user.id}> စကားပြန်ပြောလို့ရ ပြီ.`)
-                        //console.log(getStr());
+
                     }, ms(time));
                 } catch (err) {
                     console.log('error reassigning roles!!');
                     return;
-                }
-
-
-        
+                }        
             break;
         }
-
-
-
 });
 
             
